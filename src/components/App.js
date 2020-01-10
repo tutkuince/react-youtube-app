a3a2dc8a8374425db9d4161d45318ea52f5f6684
tutkuince <tutku.ince@outlook.com>
2020-01-10 13:38:18 +0300

    some bootstrap style added

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,19 @@
 import React from "react";
+import SearchBar from "./SearchBar";
 
 class App extends React.Component {
     render() {
         return(
-            <div>App</div>
+            <div className="bg-light">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <SearchBar/>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         );
     }
 }
---
+++ src/components/SearchBar.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+    state = {term: ''};
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+    };
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.onFormSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="searchTerm">Search</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="searchTerm"
+                            onChange={(event) => {
+                                this.setState({term: event.target.value})
+                            }}
+                            value={this.state.term}
+                        />
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default SearchBar;
