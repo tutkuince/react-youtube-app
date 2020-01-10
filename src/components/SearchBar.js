9b8ab44c06b8203f67b7235a8cd24b5ad4ea3149
tutkuince <tutku.ince@outlook.com>
2020-01-10 14:06:45 +0300

    term bind to App component via props

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,6 +5,8 @@ class SearchBar extends React.Component {
 
     onFormSubmit = (event) => {
         event.preventDefault();
+
+        this.props.onFormSubmit(this.state.term);
     };
 
     render() {
