edf25d999d42775d677a0ec33a1299f2ff981279
tutkuince <tutku.ince@outlook.com>
2020-01-10 14:07:20 +0300

    updating state with fetch data

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,32 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import youtube from "../apis/youtube";
 
 class App extends React.Component {
+
+    state = {
+        videos: []
+    };
+
+    onTermSubmit = async (term) => {
+        const response = await youtube.get('/search', {
+            params: {
+                q: term
+            }
+        });
+
+        this.setState({
+            videos: response
+        });
+    };
+
     render() {
         return(
             <div className="bg-light">
                 <div className="container">
                     <div className="row">
                         <div className="col-md-12">
-                            <SearchBar/>
+                            <SearchBar onFormSubmit={this.onTermSubmit}/>
                         </div>
                     </div>
                 </div>
